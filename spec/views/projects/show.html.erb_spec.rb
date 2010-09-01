6d4dc0507a354518d78517774342bff8b64c6754
Metalelf0 <metalelf0@gmail.com>
2010-09-01 08:58:25 +0200

    Added list of project iterations in project show view

--- spec/views/projects/show.html.erb_spec.rb
+++ spec/views/projects/show.html.erb_spec.rb
@@ -6,10 +6,27 @@ describe "/projects/show.html.erb" do
     assigns[:project] = @project = stub_model(Project,
       :name => "value for name"
     )
+    @iterations = [
+      stub_model(Iteration,
+                 :number => 1,
+                 :project => @project
+      ),
+      stub_model(Iteration,
+                 :number => 2,
+                 :project => @project
+      ),
+    ]
+    @project.stub!(:iterations).and_return(@iterations)
   end
 
   it "renders attributes in <p>" do
     render
     response.should have_text(/value\ for\ name/)
   end
+
+  it "should show iterations under project" do
+    render
+    response.should have_tag(".iteration_number", 2)
+  end
+
 end
