8ed1ecf9162431efb7193597b59bb1ce4cf4037d
Metalelf0 <metalelf0@gmail.com>
2010-09-05 10:49:44 +0200

    Added default route to parser/index, changed project index page

--- spec/views/projects/index.html.erb_spec.rb
+++ spec/views/projects/index.html.erb_spec.rb
@@ -5,17 +5,34 @@ describe "/projects/index.html.erb" do
 
   before(:each) do
     assigns[:projects] = [
-      stub_model(Project,
+      @project = stub_model(Project,
         :name => "value for name"
       ),
       stub_model(Project,
         :name => "value for name"
       )
     ]
+    @iterations = [
+      stub_model(Iteration,
+                 :number => 1,
+                 :project => @project
+      ),
+      stub_model(Iteration,
+                 :number => 2,
+                 :project => @project
+      ),
+    ]
+    @project.stub!(:iterations).and_return(@iterations)
   end
 
   it "renders a list of projects" do
     render
     response.should have_tag("tr>td", "value for name".to_s, 2)
   end
+  
+  it "should show iterations under project" do
+    render
+    response.should have_tag(".iteration_number", 2)
+  end
+
 end
